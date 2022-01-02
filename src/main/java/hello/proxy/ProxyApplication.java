0143085d853a5eca118cea59003f559dcf8644ac
주진성 <jjsair0412@naver.com>
2022-01-02 16:54:30 +0900

    JDK동적프록시 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,8 @@ package hello.proxy;
 
 import hello.proxy.config.ConcreteProxyConfig;
 import hello.proxy.config.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFiterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +16,9 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 주의 - 컴포넌트 스캔의 범위는 , main이 위치한 패키지에 있는 애들이랑 하위애들만 컴포넌트 스캔 대상이 되는데,
  															// 안에 저렇게 설정해주면 hello.proxy.app과 그 하위만 컴포넌트스캔 대상이 된다.
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFiterConfig.class)
 public class ProxyApplication {
 
 	public static void main(String[] args) {
