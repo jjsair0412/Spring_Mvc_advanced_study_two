dd80ac4450b1a8c23ac2f260bde062e622d172d9
주진성 <jjsair0412@naver.com>
2022-01-02 21:12:33 +0900

    JDK 동적 프록시 설명주석 작성 및 이론 복습

--- src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -11,16 +11,48 @@ public class JdkDynamicProxyTest {
 
     @Test
     void dynamicA(){
+        // interface를 객체로 만들고 , 구현체를 통해 인스턴스화 시켜준다.
         Ainterface target = new AImpl();
 
+        // 아까 jdk 동적프록시를 생성할 handler를 인스턴스화 한다.
+        // 생성될때 인자값으로 프록시설정 대상이 들어가기에 , 위에 만들어주었던 Ainterface 객체인 target을 넣어준다.
         TimeInvocationHandler handler = new TimeInvocationHandler(target);
 
-        Ainterface proxy = (Ainterface) Proxy.newProxyInstance(Ainterface.class.getClassLoader(), new Class[]{Ainterface.class}, handler);
+        /**
+         *          Proxy.newProxyInstance()를 통해 동적프록시를 생성시켜준다.
+         *          Proxy.newProxyInstance()의 인자값은 차례대로
+         *          1. 클래스 로더를 지정한다
+         *              - 프록시가 어디에 생성될지를 지정한다.
+         *          2. 들어갈 인터페이스들을 지정한다.
+         *              - 얘는 배열로 넣어주어야 해서 , new Class[]{들어갈_인터페이스} 형태로 넣는다.
+         *              왜냐면 여러개일수 있기 때문이다.
+         *          3. 프록시가 사용할 로직이 무엇인지 넣어준다.
+         *
+         *          그리고 생성된 프록시는 아까 설정한 Ainterface의 타입을 가지기에
+         *          Ainterface로 캐스팅이 가능하다.
+         *
+         *          그니까 프록시 클래스는 Proxy.newProxyInstance() 부분에서 만들어지는것이다.
+         */
+        Ainterface proxy = (Ainterface) Proxy.newProxyInstance(
+                Ainterface.class.getClassLoader(),
+                new Class[]{Ainterface.class},
+                handler);
         //해당 프록시는 handler의 로직을 수행하게 된다.
 
         proxy.call();
+        // 사용할땐 인터페이스의 메서드를 그냥 호출해주면 된다.
+        // Ainterface의 call()을 호출한다.
         log.info("targetClass={}",target.getClass());
         log.info("proxyClass={}",proxy.getClass());
+        /***
+         * 결과 :
+         * targetClass=class hello.proxy.jdkdynamic.code.AImpl
+         * proxyClass=class com.sun.proxy.$Proxy9
+         *
+         * target은 Ainterface를 통해 만들어진 객체라는걸 알 수 있으며
+         * 만들어진 proxy는 $Proxy9 라는것을 알 수 있다.
+         *
+         */
     }
 
     @Test
--- src/test/java/hello/proxy/jdkdynamic/code/TimeInvocationHandler.java
+++ src/test/java/hello/proxy/jdkdynamic/code/TimeInvocationHandler.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Method;
 @Slf4j
 public class TimeInvocationHandler implements InvocationHandler {
 
-    private final Object target;
+    private final Object target; // 프록시가 호출할 대상을 의존주입 받아준다. 데이터타입은 Object이기에 다 올수 있다.
 
     public TimeInvocationHandler(Object target){
         this.target = target;
@@ -16,11 +16,22 @@ public class TimeInvocationHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 프록시가 생성될때 공통적으로 수행할 로직 작성
         log.info("TimeProxy 실행");
         long startTime = System.currentTimeMillis();
 
+        /***
+         * 여기에 만약 Ainterface의 call() 메서드를 호출한다면 , method는 call이 될 것이다.
+         */
+
         Object result = method.invoke(target, args);
-        // 인자값으로 넘어온 메서드를 실행시키기 위해, 대상 객체와 인자값을 넘어온 메서드에 넣어준다.
+
+        /***
+         *  jdk 동적 프록시가 호출될 때 어떤 메서드가 실행될지도 넘어온다.
+         *  따라서 method.invoke()를 사용해서 메서드를 실행한다.
+         *  method.invoke()의 인자값으론 어떤 메서드가 오느냐 ( 위의 의존주입받은 호출대상 ) 을 넣어주고,
+         *  그뒤엔 인자값 args 를 넣어준다.
+         */
 
         long endTime = System.currentTimeMillis();
 
